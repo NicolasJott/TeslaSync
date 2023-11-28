1882dac5e62dc572ed0cda83d1a33881d823291d
nicolasjott <nicobox2002@gmail.com>
2023-11-28 10:35:42 -0500

    Initial Commit

---
+++ teslasync/teslasyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  teslasyncApp.swift
+//  teslasync
+//
+//  Created by Nicolas Ott on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct teslasyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
